fd4c7a28352ec607a8d0ed560b43fb4f3d5201af
rafiquie <rafiquie112@gmil.com>
2025-08-14 21:49:23 +0530

    completed the first leacture topics of js introduction and variables

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ console.log(num1)
 
 //Data types using global scope
 
-//var using  
+//var using  both can change decalaration and intialiuzation possible;
 var a="hi !";
 var a="who the hell are you";
 console.log(a)
 
 //let using
-
+//only intailzation is possible values can chage
 let b="123";
     b="456"
 console.log(b)
 
-// const using
+// const using 
 
 const id =2333263928;
 console.log(id)
@@ -24,7 +24,17 @@ console.log(id)
 //local scope block
 {
     const id=1;
+   // const id =2; decalaration aND INTIALIZTION IS not possible
     const name="Gojo";
 
     console.log(id,name);
 }
+
+//how to namming varibles
+/*1. meaningful
+2. case sensentive
+3 .start with alphabests ,in naming aphlabets ,numbers, _ ca be use
+4.numbers not start with name of variable
+5.reversed word are not use;
+6.blank sapce is n ot allowed
+*/
