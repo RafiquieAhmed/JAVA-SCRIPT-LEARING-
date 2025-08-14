9f9baefb1d2f39dfb34cc9c77e45d9868062b101
rafiquie <rafiquie112@gmil.com>
2025-08-14 16:17:49 +0530

    completed the first leacture on js series

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello_world");
+//variables 
+num1=10;
+console.log(num1)
+
+//Data types using global scope
+
+//var using  
+var a="hi !";
+var a="who the hell are you";
+console.log(a)
+
+//let using
+
+let b="123";
+    b="456"
+console.log(b)
+
+// const using
+
+const id =2333263928;
+console.log(id)
+
+//local scope block
+{
+    const id=1;
+    const name="Gojo";
+
+    console.log(id,name);
+}
